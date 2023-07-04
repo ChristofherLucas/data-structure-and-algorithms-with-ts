1d31952a5c383a951a0a6511a7e0e52c978f5507
ChristofherLucas <lucaschristofher05@gmail.com>
2023-07-04 09:13:43 -0300

    feat: add method hash

--- src/algorithms/HashTable.ts
+++ src/algorithms/HashTable.ts
@@ -4,4 +4,12 @@ export class HashTable<T> {
   constructor() {
     this.items = {};
   }
+
+  loseLoseHashCode(key: string) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+    return hash % 37;
+  }
 }
