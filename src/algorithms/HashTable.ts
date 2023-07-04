d0bc94bc1a6fad819a75421fa0b14efc1de9e1cc
ChristofherLucas <lucaschristofher05@gmail.com>
2023-07-04 09:22:12 -0300

    feat: add method put

--- src/algorithms/HashTable.ts
+++ src/algorithms/HashTable.ts
@@ -1,15 +1,25 @@
+import { ValuePair } from "../utils/ValuePair";
+
 export class HashTable<T> {
-  private items: { [key: string]: T };
+  private table: { [key: string]: ValuePair<T> };
 
   constructor() {
-    this.items = {};
+    this.table = {};
   }
 
-  loseLoseHashCode(key: string) {
+  private loseLoseHashCode(key: string) {
     let hash = 0;
     for (let i = 0; i < key.length; i++) {
       hash += key.charCodeAt(i);
     }
     return hash % 37;
+  }
+
+  put(key: string, value: T) {
+    if (key != null && value != null) {
+      const position = this.loseLoseHashCode(key);
+      this.table[position] = new ValuePair(key, value);
+    }
+    return false;
   }
 }
